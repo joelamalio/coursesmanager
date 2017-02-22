559715897898336b608e89281047924fe8cc8cfb
Joel Amálio <joel.amalio@gmail.com>
2017-02-22 02:33:15 -0300

    Project configuration to run in Weblogic 12c.

--- coursesmanager-web/src/main/java/br/com/jamc/coursesmanager/CoursesManagerApplication.java
+++ coursesmanager-web/src/main/java/br/com/jamc/coursesmanager/CoursesManagerApplication.java
@@ -2,12 +2,21 @@ package br.com.jamc.coursesmanager;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
 
+@ComponentScan   
 @SpringBootApplication
-public class CoursesManagerApplication {
+public class CoursesManagerApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(CoursesManagerApplication.class, args);
-    }
+   public static void main(String[] args) {
+      SpringApplication.run(CoursesManagerApplication.class, args);
+   }
 
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(CoursesManagerApplication.class);
+   }
 }
