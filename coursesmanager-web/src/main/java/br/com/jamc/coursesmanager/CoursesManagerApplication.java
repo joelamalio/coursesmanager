f8406c38537611cde313e6c1caffa61405c41750
Joel Amálio <joel.amalio@gmail.com>
2017-03-02 02:47:23 -0300

    Annotation removal.

--- coursesmanager-web/src/main/java/br/com/jamc/coursesmanager/CoursesManagerApplication.java
+++ coursesmanager-web/src/main/java/br/com/jamc/coursesmanager/CoursesManagerApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.WebApplicationInitializer;
 
-@ComponentScan   
 @SpringBootApplication
 public class CoursesManagerApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
